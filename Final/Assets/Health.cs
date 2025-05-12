74565f1220c3f5477e4429b41792ca5d068df1c3
DrChickenStrips <hunte491@minidokaschools.org>
2025-05-12 12:06:47 -0600

    Player Health

---
+++ Final/Assets/Health.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public TMP_Text healthGUI;
+    public Transform player;
+    public int currentHealth;
+    public int maxHealth = 100;
+    public GameObject deathEffect;
+    public int damage = 40;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+    }
+void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        Enemy enemy = hitInfo.GetComponent<Enemy>();
+        if (enemy != null)
+        {
+            //player.TakeDamage(damage); fix TakeDamage
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthGUI.text = currentHealth.ToString();
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        //Instantiate(deathEffect, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}
